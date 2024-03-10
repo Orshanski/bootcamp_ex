9b8cc259d3d08a0a2fd431eca57c70172e2e5e2d
Alexey Orshanski <alexey.orshanski@gmail.com>
2024-03-10 20:25:05 +0200

    loop1 and move files in folder mondey

---
+++ week03/monday/loop1.js
@@ -0,0 +1,18 @@
+const names = ["Ashley", "Donovan", "Lucas"]
+const ages = [23, 47, 18]
+const people = []
+for (let i in names) {
+    names[i] = { name: names[i], age: ages[i] };
+}
+
+const nm = document.getElementById("names")
+
+function displayNames() {    
+    for (let i in names) {
+        const nameElement = document.createElement("li");
+        nameElement.innerHTML = `${names[i].name} is ${names[i].age}`;
+        nm.appendChild(nameElement);
+    }
+}
+
+displayNames();
