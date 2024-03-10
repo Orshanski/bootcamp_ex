626ff24205701747a9b5ca5d17cbe76c73b465a7
Alexey Orshanski <alexey.orshanski@gmail.com>
2024-03-10 19:48:04 +0200

    library

---
+++ week03/library.js
@@ -0,0 +1,22 @@
+const library = {
+    books: [
+        { title: "Pride and Prejudice", author: "Jane Austen" },
+        { title: "1984", author: "George Orwell" },
+        { title: "Hamlet", author: "William Shakespeare" },
+        { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+        { title: "To Kill a Mockingbird", author: "Harper Lee" }
+    ]
+};
+const books = document.getElementById("books");
+
+function displayBooks() {
+    for (let i = 0; i < library.books.length; i++) {
+        const book = library.books[i];
+        const bookElement = document.createElement("li");
+        bookElement.innerHTML = `${book.title} by ${book.author}`;
+        books.appendChild(bookElement);
+    }
+}
+
+
+displayBooks();
