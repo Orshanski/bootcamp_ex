6fba925ed7b46fda95b77f90745733886f6c5e71
Alexey Orshanski <alexey.orshanski@gmail.com>
2024-03-13 13:20:22 +0200

    Fix errors in ex23.js

--- week03/omerJavaScriptExercisesPart2/ex23.js
+++ week03/omerJavaScriptExercisesPart2/ex23.js
@@ -1,17 +1,18 @@
-let originalArray = 
-    document.getElementById('original_array').value.split(/[ ,;]+/).map(Number);
-
-
-let newArray = []
+function originalArray() {
+    let originalArray =
+        document.getElementById('original_array').value.split(/[ ,;]+/).map(Number);
+    return originalArray;
+}
 
 //I am writing the function only because it is specified in the exercise.
 //I know that I can use the Set object directly
-function removeDuplicates(arr){
+function removeDuplicates(arr) {
     newArray = [...new Set(arr)];
+    return newArray;
 }
 
-document.getElementById('get_new_array').addEventListener('click', function(){
-    removeDuplicates(originalArray);
+document.getElementById('get_new_array').addEventListener('click', function () {
+    originalArray();
+    newArray = removeDuplicates(originalArray());
     document.getElementById('unique_array').innerHTML = newArray;
 });
-
