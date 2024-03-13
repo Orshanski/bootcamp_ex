cd8005df7e7ba67c5b5b9e87bc2ea77b987553c9
Alexey Orshanski <alexey.orshanski@gmail.com>
2024-03-13 13:38:11 +0200

    Made more elegant solution for ex8.js

--- week03/omerJavaScriptExercises/ex8.js
+++ week03/omerJavaScriptExercises/ex8.js
@@ -10,6 +10,7 @@ function getString(country, population, percentage) {
     return `${country} has ${population} million people and its population is about ${percentage}% of the world population.`;
 }
 
+
 function percentageOfWorld1(population) {
     const worldPopulation = 7900; // assuming world population is 7.9 billion
     const percentage = (population / worldPopulation) * 100;
@@ -22,41 +23,16 @@ const percentageOfWorld2 = function (population) {
     return percentage.toFixed(1);
 }
 
-function saveInfo1() {
+function countryInfo1() {
     let [country, population] = getInfo();
     let info = getString(country, population, percentageOfWorld1(population));
-    let countries = JSON.parse(localStorage.getItem('countries')) || [];
-    countries.push(info);
-    localStorage.setItem('countries', JSON.stringify(countries));
-}
-
-function saveInfo2() {
-    
-    let [country, population] = getInfo();
-    let info = getString(country, population, percentageOfWorld2(population));
-    let countries1 = JSON.parse(localStorage.getItem('countries1')) || [];
-    countries1.push(info);
-    localStorage.setItem('countries1', JSON.stringify(countries1));
-}
-
-function countryInfo1() {
-    saveInfo1();
-    let countries = JSON.parse(localStorage.getItem('countries')) || [];
-    let result = '';
-    for (let country of countries) {
-        result += country + '<br>';
-    }
-    document.getElementById('resultFn1').innerHTML = result;
+    document.getElementById('resultFn1').innerHTML += info + '<br>';
 }
 
 function countryInfo2() {
-    saveInfo2();
-    let countries = JSON.parse(localStorage.getItem('countries1')) || [];
-    let result = '';
-    for (let country of countries) {
-        result += country + '<br>';
-    }
-    document.getElementById('resultFn2').innerHTML = result;
+    let [country, population] = getInfo();
+    let info = getString(country, population, percentageOfWorld1(population));
+    document.getElementById('resultFn2').innerHTML += info + '<br>';
 }
 
 function clearInfo() {
