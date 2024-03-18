fe51b208effe9273c5e4cc275954073734eb545d
Alexey Orshanski <alexey.orshanski@gmail.com>
2024-03-18 19:47:33 +0200

    Fix ex14.js and ex14.html

--- week03/omerJavaScriptExercises/ex14.js
+++ week03/omerJavaScriptExercises/ex14.js
@@ -1,20 +1,27 @@
-function capitalizeFirstLetter(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }
+document.getElementById('submit').addEventListener('click', function () {
 
-function countryToLiveIn(language, isIsland, population, country) {
-    let language = document.getElementById('language').value;
-    let isIsland = document.getElementById('isIsland').checked;
-    let population = document.getElementById('population').value;
-    let country = document.getElementById('country').value;
-    country = capitalizeFirstLetter(country);
+    const language = document.getElementById('language').value;
+    const isIsland = document.getElementById('isIsland').checked;
+    const population = document.getElementById('population').value;
+    const country = document.getElementById('country').value;
 
-    answer = '';
-    if (language === 'english' && population < 50 && !isIsland) {
-        answer = `You should live in <b>${country}</b>`;
-    } else {
-        answer = `<b>${country}</b> does not meet your criteria`;
+    countryToLiveIn(language, isIsland, population, country);
+
+    function countryToLiveIn(language, isIsland, population, country) {
+        console.log(language, isIsland, population, country);
+        country = capitalizeFirstLetter(country);
+
+        function capitalizeFirstLetter(word) {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        }
+
+        answer = '';
+        if (language === 'english' && population < 50 && !isIsland) {
+            answer = `You should live in <b>${country}</b>`;
+        } else {
+            answer = `<b>${country}</b> does not meet your criteria`;
+        }
+
+        document.getElementById('result').innerHTML = answer;
     }
-
-    document.getElementById('result').innerHTML = answer;
-}
+})
