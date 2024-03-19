c23c9e0d520244530c34cfd1bfeb2ac18b883c9a
Alexey Orshanski <alexey.orshanski@gmail.com>
2024-03-19 16:55:13 +0200

    fix the ex20.js file

--- week03/omerJavaScriptExercisesPart2/ex20.js
+++ week03/omerJavaScriptExercisesPart2/ex20.js
@@ -2,7 +2,6 @@ function originalArray() {
     //protecting from leading and trailing spaces
     let str = document.getElementById('original_array').value.trim();
     let originalArray = str.split(/[ ,;]+/)
-    console.log(originalArray);
     return originalArray;
 }
 
@@ -10,7 +9,7 @@ document.getElementById('get_result_array').addEventListener("click", main)
 
 function main() {
 
-    let result = [];
+    const result = [];
     let original_array = originalArray();
     original_array.forEach(element => {
         result.push(element.length);
